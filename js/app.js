fef298c31362b588dc288d4c1757b4eeba313c18
Edgardo Rito Deheza <edgardorito@gmail.com>
2018-03-07 10:57:01 -0600

    Uso de var y let

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log("Hola mundo");
+
+function variable_local(edad) {
+  //scope local
+  var resultado; //var se puede usar la variable en cualquier parte de la función
+  if (edad >= 18) {
+    resultado = "Es mayor de edad";
+  }else {
+    resultado = "Es menor de edad";
+  }
+  console.log(resultado);
+}
+
+function variable_local2(edad) {
+  let resultado; // Si se utiliza de esta manera, se hace referencia al scope global
+  if (edad >= 18) {
+    resultado = "Es mayor de edad"; //let solo se puede utilizar en la estructura que esta declarada
+  }else {
+    resultado = "Es menor de edad";
+  }
+  console.log(resultado);
+}
+//variable_local();
+variable_local2()
